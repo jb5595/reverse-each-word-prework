a8d46593b4ad7a12f18439e984b54fcd06e88c1d
Jacob Berman <jacobberman1995@gmail.com>
2018-08-08 21:36:32 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word(string)
+  reverse = []
+  array = string.split(" ")
+  array.collect do |word|
+    reverse_word = []
+    letters = word.split("")
+    letters.each do |letter|
+      reverse_word.unshift(letter)
+    end
+    reverse.push(reverse_word.join)
+  end
+reverse.join(" ")
+  
+end 
