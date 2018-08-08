528467f3cd24f891f58a6fe8376fdd7fb90a0fa9
Jacob Berman <jacobberman1995@gmail.com>
2018-08-08 21:33:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(string)
   reverse = []
   array = string.split(" ")
-  array.each do |word|
+  array.collect do |word|
     reverse_word = []
     letters = word.split("")
-    letters.each do |letter|
+    letters.collect do |letter|
       reverse_word.unshift(letter)
     end
     reverse.push(reverse_word.join)
