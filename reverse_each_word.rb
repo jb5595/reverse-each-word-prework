964f5e85d7a37ed88c0294aba45d065f67bf2a61
Jacob Berman <jacobberman1995@gmail.com>
2018-08-08 21:28:14 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   reverse = []
   array.each do |word|
-    reverse.push(word)
+    reverse.unshift(word)
   end
   return reverse.join(" ")
   
